2a45a615409490a4b8ea686fa29daf99970dd37f
Rodolfo Vázquez <vazquezrrodolfo@gmail.com>
2020-10-23 00:18:01 -0500

    Regular Expressions. Thursday update

---
+++ RE/Program.fs
@@ -0,0 +1,19 @@
+﻿(*
+ * *****************************************************************************
+ *        Name:    Rodolfo Emanuel   
+ *     Surname:    Vázquez Reyes
+ *          ID:    171072
+ *       Major:    IT Engineering
+ * Institution:    Universidad Politécnica de San Luis Potosí
+ *   Professor:    Juan Carlos González Ibarra
+ * Description:    F# language program to show Regular Expressions
+ *                
+ *  Written:       22/10/2020
+ *  Last updated:  22/10/2020
+ **************************************************************************** *)
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
